ebbd943a62e04c30233e0295f52f375cded63d00
Optimus-pr <yaagamilight65@gmail.com>
2023-05-14 18:07:03 +0530

    image added to home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand fs-1 fst-italic" to="/">SMS</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import img from '../images/lib.jpg'
 
 export default function Home() {
   return (
     <div>
         <Navbar/>
-        <div style={{textAlign:"center",margin:"100px",fontSize:"2.5rem"}}>
+        {/* <div style={{textAlign:"center",margin:"100px",fontSize:"2.5rem"}}>
           <h1>WELCOME TO STUDENT MONITORING SYSTEM</h1>
+        </div> */}
+        <div style={{height:'900px',width:'1500px'}}>
+          <img src={img} alt="" style={{height:'100%',width:'100%',objectFit:'cover'}}/>
         </div>
     </div>
   )
